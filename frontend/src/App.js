7dd73b8591ef5639e070b9676401f1bed8c4e5d5
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-09-06 16:58:22 +0100

    added tailwind plugins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-7xl font-roboto ">Hello world!</h1>
     </div>
   );
 }
