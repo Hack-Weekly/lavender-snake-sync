0b504bbc04a8079c83ce18c5b836cfe7f8d59db2
Nife <nifeakin@icloud.com>
2023-09-06 20:08:34 -0400

    added CRUD prototype for events

--- api/urls.py
+++ api/urls.py
@@ -16,5 +16,8 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
     path('test/', views.testEndPoint, name='test'),
+    path('allevents/',views.EventView.as_view()),
+    path('event/',views.CrudEventView.as_view(), name="new event"),
+    path('event/<int:pk>/',views.CrudEventView.as_view(), name="event")
     #project urls
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse,Http404
-from api.serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from api.serializers import MyTokenObtainPairSerializer, RegisterSerializer, EventSerializer, CreateEventSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from django.contrib.auth import get_user_model
@@ -11,6 +11,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
 from django.views.generic import TemplateView
+from .models import Event
 
 User=get_user_model()
 # project imports
@@ -30,7 +31,9 @@ def getRoutes(request):
         '/api/',
         '/api/token/',
         '/api/register/',
-        '/api/token/refresh/',     
+        '/api/token/refresh/',
+          
+
     ]
    
     return Response(routes)
@@ -55,3 +58,65 @@ def testEndPoint(request):
             return Response("Invalid JSON data", status.HTTP_400_BAD_REQUEST)
     return Response("Invalid JSON data", status.HTTP_400_BAD_REQUEST)
 
+class EventView(generics.ListAPIView):
+    #shows all events
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+#@permission_classes([IsAuthenticated])
+class CrudEventView(APIView):
+    serializer_class = EventSerializer
+    def post(self,request,format=None):
+        '''
+        Create new event and check that inputs are valid
+        '''
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            is_public = serializer.data.get("is_public")
+            event_start = serializer.data.get("event_start")
+            event_end = serializer.data.get("event_end") 
+            user = User.objects.get(user_name=self.request.user.username)
+            event_note = serializer.data.get("event_note")
+            anonymous = serializer.data.get("anonymous")
+            event = Event(creator=user,is_public=is_public,event_start=event_start,event_end=event_end,event_note=event_note,anonymous=anonymous)
+            event.save()
+            return Response({"event_id":event.id},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self,request,pk=None,format=None):
+        '''Retrieve an event using unique id:pk
+           If user did not create the event, then note cannot be viewed
+           If the event is anonymous then name and notes cannot be viewed
+        '''
+        if not pk:
+            return Response({"message":"Welcome, create a new event",},status=status.HTTP_200_OK)
+        event = Event.objects.get(pk=pk)
+        if event.creator == request.user:
+            event = Event.objects.get(pk=pk)
+        if event.anonymous and event.creator != request.user:
+            event = Event.objects.filter(pk=pk).defer("event_note","name")[0]
+        if event.creator != request.user and not event.anonymous:
+            event = Event.objects.filter(pk=pk).defer("event_note")[0]
+        serializer = self.serializer_class(event)
+        return Response(serializer.data)
+    
+    def patch(self,request,pk,format=None):
+        '''Update an existing event created by the user'''
+        query_set = Event.objects.filter(pk=pk)
+        serializer = self.serializer_class(data=request.data)
+        if query_set.exists():
+            event = query_set[0]
+            print(event.creator,self.request.user)
+            if event.creator != self.request.user:
+                return Response({"message":"You did not create this event"},status=status.HTTP_401_UNAUTHORIZED)
+            if serializer.is_valid():
+                event.is_public = serializer.data.get("is_public")
+                event.event_start = serializer.data.get("event_start")
+                event.event_end = serializer.data.get("event_end")
+                event.event_note = serializer.data.get("event_note")
+                event.anonymous = serializer.data.get("anonymous")
+                event.save(update_fields=["is_public","event_start","event_end","event_note","anonymous"])
+                return Response({"message":"Successfully updated"},status=status.HTTP_202_ACCEPTED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"Event Error"},status=status.HTTP_400_BAD_REQUEST)
