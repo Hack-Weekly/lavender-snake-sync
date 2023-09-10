ac66a6100d4a4934afb4c9402c477cf79dcee9a6
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-09-10 10:35:38 +0100

    Fixed landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import { LandingText } from "../components/LandingText"
 import { TopNavbar } from "../components/TopNavbar"
 export const LandingPage = () => {
     return(
-        <div className="h-full w-screen dark:bg-gray-900">
+        <div className="h-screen w-screen dark:bg-gray-900">
             <TopNavbar />
             <LandingText />
         </div>
