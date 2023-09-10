ad65c08b22778425cbe47cb9c8ab6f27c8f614b5
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-09-10 16:46:18 +0100

    Added API connections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import {Route, Routes } from "react-router-dom";
 import { LandingPage } from './pages/LandingPage';
+import { LoginPage } from './pages/LoginPage';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
