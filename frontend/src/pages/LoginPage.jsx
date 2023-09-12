c804d09cd478c8942d2bb49860716d4d40cbcad5
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-09-12 12:20:48 +0100

    Added login/signup logic

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Login } from '../components/Login';
 import { ImageAuth } from '../components/ImageAuth';
-
+import { login } from '../services/endpoints/users';
 export const LoginPage = () => {
+    const [failureLogin, setFailureLogin] = useState(false);
+    const handleLogin = async(username, password) => {
+        const credentials  = {username, password};
+        try {
+            await login(credentials);
+        }catch (error) {
+            console.error(error);
+            setFailureLogin(true);
+        }
+    }
     return (
         <div className="flex h-screen dark:bg-slate-900">
             <div className="w-1/2 bg-gradient-to-r from-purple-500 to-purple-700 flex justify-center items-center">
@@ -10,7 +20,7 @@ export const LoginPage = () => {
             </div>
             <div className="w-1/2 flex flex-col items-center justify-center">
                 <h1 className='text-4xl font-semibold dark:text-white'>Login</h1>
-                <Login />
+                <Login handleLogin={handleLogin} failureLogin={failureLogin} setFailureLogin={setFailureLogin}/>
             </div>
         </div>
     );
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import { useState } from "react";
 import { ImageAuth } from '../components/ImageAuth';
+import { register } from '../services/endpoints/users';
 import { Signup } from '../components/Signup';
 
 export const SignupPage = () => {
+    const [failureSignup, setFailureSignup] = useState(false);
+
+    const handleSignup = async(username, password, email) => {
+        const credentials  = {username, password, email, password2 : password};
+        try {
+            await register(credentials);
+        }catch (error) {
+            console.error(error);
+            setFailureSignup(true);
+        }
+    }
     return (
         <div className="flex h-screen dark:bg-slate-900">
             <div className="w-1/2 bg-gradient-to-r from-purple-500 to-purple-700 flex justify-center items-center">
@@ -10,7 +22,7 @@ export const SignupPage = () => {
             </div>
             <div className="w-1/2 flex flex-col items-center justify-center">
                 <h1 className='text-4xl font-semibold dark:text-white'>Sign up</h1>
-                <Signup />
+                <Signup handleSignup={handleSignup} failureSignup={failureSignup} setFailureSignup={setFailureSignup}/>
             </div>
         </div>
     );
